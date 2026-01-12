6831b334194c8acdff1ee2b55919fc69511a9c13
Ivaylo Manev <ivaylo_manev@abv.bg>
2026-01-12 17:55:01 +0200

    Changes to the UI - needs rework

--- DentistAppointmentApp/DentistApp.Data/DentistDbContext.cs
+++ DentistAppointmentApp/DentistApp.Data/DentistDbContext.cs
@@ -1,9 +1,9 @@
 ﻿namespace DentistAppointmentApp.Data
 {
-
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+
+    public class DentistDbContext(DbContextOptions<DentistDbContext> options) : IdentityDbContext(options)
     {
     }
 }
