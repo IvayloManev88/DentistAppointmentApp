22117f9eb02d760bdcf976b895b20905015ad445
Ivaylo Manev <ivaylo_manev@abv.bg>
2026-01-16 22:03:37 +0200

    Fixing bugs - DbContext

--- DentistAppointmentApp/DentistApp.Data/DentistAppDbContext.cs
+++ DentistAppointmentApp/DentistApp.Data/DentistAppDbContext.cs
@@ -6,13 +6,18 @@
     using Microsoft.EntityFrameworkCore;
     using System.Reflection;
 
-    public class DentistAppDbContext(DbContextOptions<DentistAppDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class DentistAppDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DentistAppDbContext(DbContextOptions<DentistAppDbContext> options)
+        : base(options)
+        {
+
+        }
         public virtual DbSet<ManipulationType> ManipulationTypes { get; set; } = null!;
         public virtual DbSet<Procedure> Procedures { get; set; } = null!;
         public virtual DbSet<Reservation> Reservations { get; set; } = null!;
 
-        
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
