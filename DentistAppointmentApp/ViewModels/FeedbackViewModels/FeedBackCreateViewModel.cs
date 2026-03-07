a219c26126fc62adb1242db1ea5dfe75df5c7163
Ivaylo Manev <ivaylo_manev@abv.bg>
2026-03-07 23:17:19 +0200

    View models for Feedback Creation

---
+++ DentistAppointmentApp/ViewModels/FeedbackViewModels/FeedBackCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DentistApp.ViewModels.FeedbackViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+    using static DentistApp.GCommon.EntityConstants.Feedback;
+    public class FeedBackCreateViewModel
+    {
+        [StringLength(FeedbackTextMaxLength, MinimumLength =FeedbackTextMinLength,
+            ErrorMessage = "{0} must be between {2} and {1} characters.")]
+        public string FeedbackText { get; set; } = null!;
+
+        [Range(FeedbackMinRating, FeedbackMaxRating,
+            ErrorMessage = "{0} must be between {1} and {2}.")]
+        public int Rating { get; set; }
+    }
+}
