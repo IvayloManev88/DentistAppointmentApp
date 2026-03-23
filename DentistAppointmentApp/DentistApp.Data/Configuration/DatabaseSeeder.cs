b9c2b8dcb30211d977fcaed701b4275c1b7a107a
Ivaylo Manev <ivaylo_manev@abv.bg>
2026-03-23 11:03:43 +0200

    Seeding Users

--- DentistAppointmentApp/DentistApp.Data/Configuration/DatabaseSeeder.cs
+++ DentistAppointmentApp/DentistApp.Data/Configuration/DatabaseSeeder.cs
@@ -4,7 +4,8 @@
 
     using Microsoft.AspNetCore.Identity;
     using Microsoft.Extensions.DependencyInjection;
-    
+    using DentistApp.Data.Models;
+
     public static class DatabaseSeeder
     {
         public static void SeedRoles(IServiceProvider serviceProvider)
@@ -37,6 +38,91 @@
             }
         }
 
+        public static async Task SeedUsersAsync(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            //Seed Dentist user - important because my application uses one dentist user and if there is no seed then it will have error exceptions thrown
 
+            ApplicationUser? dentistUser = await userManager.FindByEmailAsync("TestDentist@abv.bg");
+
+            if (dentistUser == null)
+            {
+                dentistUser = new ApplicationUser
+                {
+                    UserName = "TestDentist@abv.bg",
+                    Email = "TestDentist@abv.bg",
+                    FirstName = "Kristina",
+                    LastName = "Maneva",
+                    EmailConfirmed = true
+                };
+                //Just for exam purposes the passwords are easy. On prod they will be different 
+                var result = await userManager.CreateAsync(dentistUser, "123456");
+
+                if (!result.Succeeded)
+                {
+                    throw new Exception();
+                }
+
+                await userManager.AddToRoleAsync(dentistUser, DentistRoleName);
+            }
+
+            //Seed normal users
+            ApplicationUser[] users = new ApplicationUser[]
+            {
+                new ApplicationUser
+                 {
+                    UserName = "ivo@abv.bg",
+                    Email = "ivo@abv.bg",
+                    FirstName = "Ivaylo",
+                    LastName = "Manev",
+                    EmailConfirmed = true
+                },
+
+                 new ApplicationUser
+                 {
+                    UserName = "pesho@abv.bg",
+                    Email = "pesho@abv.bg",
+                    FirstName = "Petur",
+                    LastName = "Petrov",
+                    EmailConfirmed = true
+                },
+
+                  new ApplicationUser
+                 {
+                    UserName = "Gorgi@abv.bg",
+                    Email = "Gorgi@abv.bg",
+                    FirstName = "Georgi",
+                    LastName = "Georgiev",
+                    EmailConfirmed = true
+                },
+
+                   new ApplicationUser
+                 {
+                    UserName = "Milen@abv.bg",
+                    Email = "Milen@abv.bg",
+                    FirstName = "Milen",
+                    LastName = "Milenov",
+                    EmailConfirmed = true
+                }
+
+            };
+            foreach (var user in users)
+            {
+                ApplicationUser? normalUser = await userManager.FindByEmailAsync(user.Email!);
+                if (normalUser == null)
+                {
+                    normalUser = user;
+
+                    var result = await userManager.CreateAsync(normalUser, "123456");
+
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception();
+                    }
+
+                    await userManager.AddToRoleAsync(dentistUser, UserRoleName);
+                }
+            }
+        }
     }
 }
--- DentistAppointmentApp/DentistAppointmentApp/Program.cs
+++ DentistAppointmentApp/DentistAppointmentApp/Program.cs
@@ -53,6 +53,7 @@ using (var scope=app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     DatabaseSeeder.SeedRoles(services);
+    await DatabaseSeeder.SeedUsersAsync(services);
 }
 
 // Configure the HTTP request pipeline.
